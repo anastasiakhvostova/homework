096afa0a8c574a0082c3f3e50e4298cf795bbce7
Your Name <you@example.com>
2023-07-19 20:44:47 +0300

    third_try

--- password.py
+++ password.py
@@ -2,13 +2,13 @@ from string import ascii_lowercase, ascii_uppercase, digits
 symbols = '+-/*!"№;%:?*()'
 
 
-def are_lowercase_latter(my_password: str) -> bool:
+def are_lowercase_letter(my_password: str) -> bool:
     for mark in my_password:
         if mark in ascii_lowercase:
             return True
 
 
-def are_uppercase_latter(my_password: str) -> bool:
+def are_uppercase_letter(my_password: str) -> bool:
     for mark in my_password:
         if mark in ascii_uppercase:
             return True
@@ -27,7 +27,7 @@ def are_symbol(my_password: str) -> bool:
 
 
 def is_password_correct(my_password: str) -> bool:
-    d = are_symbol(my_password), are_digits(my_password), are_lowercase_latter(my_password), are_uppercase_latter(my_password)
+    d = are_symbol(my_password), are_digits(my_password), are_lowercase_letter(my_password), are_uppercase_letter(my_password)
     if len(my_password) >= 8:
         return all(d)
     else:
--- tests/test_password.py
+++ tests/test_password.py
@@ -2,14 +2,14 @@ import pytest
 import password
 
 
-my_password = [('nasTya9-', True),
-               ('nastya9+', False),
-               ('NASTYA7-', False),
-               ('NAStYA-+', False),
-               ('NAStYa876', False),
-               ('nAst8-', False)]
+choices = [('nasTya9-', True),
+           ('nastya9+', False),
+           ('NASTYA7-', False),
+           ('NAStYA-+', False),
+           ('NAStYa876', False),
+           ('nAst8-', False)]
 
 
-@pytest.mark.parametrize('value, result', my_password)
+@pytest.mark.parametrize('value, result', choices)
 def test_password(value, result):
     assert password.is_password_correct(value) is result
